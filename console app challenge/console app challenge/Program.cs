b5b48dcf0b3d7aaa8b00bfa8e03a541da8edea59
landen <baileylanden9@gmail.com>
2026-03-28 07:04:44 -0500

    another challenge

---
+++ console app challenge/console app challenge/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace consoleappchallenge
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly rate?");
+            decimal rate1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("How many hours per week?");
+            decimal hours1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly rate?");
+            decimal rate2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("How many hours per week?");
+            decimal hours2 = Convert.ToDecimal(Console.ReadLine());
+
+            decimal salary1 = rate1 * hours1 * 52;
+            decimal salary2 = rate2 * hours2 * 52;
+
+            Console.WriteLine("Annual salary for Person 1");
+            Console.WriteLine("salary1");
+
+            Console.WriteLine("Annual salary for Person 2");
+            Console.WriteLine("salary2");
+
+            Console.WriteLine("Does Person 1 make more than Pesrson 2?");
+            Console.WriteLine(salary1 > salary2);
+
+            Console.ReadLine();
+        }
+    }
+}
