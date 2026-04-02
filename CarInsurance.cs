174e72e5db415a55d7e9c058b18ccdddc5239f4b
landen <baileylanden9@gmail.com>
2026-04-02 09:34:15 -0500

    Boolean Logic

---
+++ CarInsurance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? (true/false)");
+            bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            bool isQualified = age > 15 && !hasDUI && speedingTickets <= 3;
+
+            Console.WriteLine("Qualified? " + isQualified);
+            Console.ReadLine();
+        }
+    }
+}
